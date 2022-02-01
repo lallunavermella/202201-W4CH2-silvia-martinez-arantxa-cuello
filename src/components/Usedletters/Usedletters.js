b20bf45a11a23cf2919237949482d241f6f69f60
Silvia Martinez <sil@silvia.com>
2022-02-01 18:50:29 +0100

    Update components

---
+++ src/components/Usedletters/Usedletters.js
@@ -0,0 +1,10 @@
+const UsedLetters = () => {
+  return (
+    <ul class="used-letters">
+      <li class="used-letter">B,&nbsp;</li>
+      <li class="used-letter">B,&nbsp;</li>
+      <li class="used-letter">B</li>
+    </ul>
+  );
+};
+export default UsedLetters;
