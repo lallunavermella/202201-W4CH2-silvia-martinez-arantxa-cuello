d8c355b3f0937703fd44091f2a97f7552d5f4ab8
Silvia Martinez <sil@silvia.com>
2022-02-01 22:50:25 +0100

    Update component Hangman and fix bug in map method in Guessword and UsedLetter

--- src/components/Guessword/Guessword.js
+++ src/components/Guessword/Guessword.js
@@ -1,15 +1,10 @@
-// props = { word: "ESPIA" }
-
-// let {word} = props
-// let word = props.word
-
 const Guessword = ({ word }) => {
   const letters = word.split("");
 
   return (
     <ul className="guess-letters">
-      {letters.map((letter) => (
-        <li className="guess-letter empty"></li>
+      {letters.map((letter, index) => (
+        <li className="guess-letter empty" key={index}></li>
       ))}
     </ul>
   );
